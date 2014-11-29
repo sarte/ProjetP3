0b464f07fe08aff80b07e11710f0bb5b9bcf1270
cmomin <charles.momin@student.uclouvain.be>
2014-11-29 15:28:06 +0100

    Update

---
+++ Projet/tache2/syntheseRe.m
@@ -0,0 +1,16 @@
+function  [NH3Pf] = syntheseRe(N,H,A,T,p,NH3P,Iv,It,Nit,x,y)
+if It==Nit
+    plot(x,y);
+    NH3Pf=Iv; 
+else
+    newN = real(N-((10^6)*((synthese(N,H,A,T,p))/(2*17))));
+    newH = real(H-(3*(10^6)*(synthese(N,H,A,T,p))/(2*17)));
+    newIv=real(Iv+(synthese(N,H,A,T,p)));
+    y(It)=Iv;
+    x(It)=It;
+    %display(['Avant:' num2str(Iv)]);
+    %display(['Après:' num2str(newIv)]);
+    NH3Pf=syntheseRe(newN, newH,A,T,p,NH3P,newIv,It+1,Nit,x,y);
+end
+end
+
