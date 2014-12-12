444e630e1541b62abd614bf67bfb9cde21594037
cmomin <charles.momin@student.uclouvain.be>
2014-12-12 19:29:36 +0100

    Update syntheseRecursion
    J'ai corrige: un argument est juste util pour faire un graphe de
    rendement, je l'ai supprime car il était plus utilisé dans ce cas-ci. +
    correction de It => 0-> 1

--- Projet/tache2/syntheseRecursion.m
+++ Projet/tache2/syntheseRecursion.m
@@ -1,18 +1,16 @@
-function  [NH3Pf] = SyntheseRecursion(N,H,A,T,p,NH3P,Iv,It,Nit,x,y)
+function  [NH3Pf] = SyntheseRecursion(N,H,A,T,p,Iv,It,Nit,x,y)
 % SyntheseRecursion, calculates the molar flow of NH3 produced in function of the following parameters:
 %
 % N = molar flow of N2 entering the reactor [mol/day]
 % H = molar flow of H2 entering the reactor [mol/day]
 % A = molar flow of Ar entering the reactor [mol/day]
 % T = temperature of the reactor [K]
-% p = pressure in the reacotor [bar]
-% NH3P = molar flow of Ar entering the reactor [mol/day]
+% p = pressure in the reactor [bar]
 % Iv = accumulator. 
-% It = iterator. Must begin with 0.
+% It = iterator. Must begin with 1.
 % Nit = requested number of iterations
 % x = x-axis vector for the plot
 % y = y-axis vector for the plot
-%
 % Calls the function synthese, who calcuates the NH3 molar flow in function
 % of the reagents, in function of the equilibrum constant. (not provided)
 
@@ -25,7 +23,7 @@ else
     newIv=real(Iv+(synthese(N,H,A,T,p)));
     y(It)=Iv;
     x(It)=It;
-    NH3Pf=syntheseRecursion(newN, newH,A,T,p,NH3P,newIv,It+1,Nit,x,y);
+    NH3Pf=syntheseRecursion(newN, newH,A,T,p,newIv,It+1,Nit,x,y);
 end
 end
 
